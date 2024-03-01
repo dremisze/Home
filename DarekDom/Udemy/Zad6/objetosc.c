547fba72d6475e6ead8891f65cd02eb7a551ef6a
dremisze <darekremiszewski@wp.pl>
2024-03-01 19:16:26 +0100

    a

---
+++ DarekDom/Udemy/Zad6/objetosc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int pole;
+    int objetosc;
+    a = 4;
+    
+    pole = 6 * (a * a);
+    objetosc = a * a * a;
+    
+    printf("Pole wynosi: %d\n", pole);
+    printf("Objetosc wynosi: %d", objetosc);
+    
+    return 0;
+}
